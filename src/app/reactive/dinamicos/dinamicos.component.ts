7ee6059bf445904d263e24d88f8d19721be05fb8
AlexGiuliano20 <alex.giuliano37@gmail.com>
2022-06-26 01:22:10 -0300

    formularios reactivos básicos y dinámicos

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,16 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormArray,
+  FormControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
+  miFormulario: FormGroup = this._fb.group({
+    nombre: [, [Validators.required, Validators.minLength(3)]],
+    favoritos: this._fb.array(
+      [
+        ['Red Dead Redemption 2', Validators.required],
+        ['Counter Strike: Global Offensive', Validators.required],
+      ],
+      Validators.required
+    ),
+  });
 
-  constructor() { }
+  nuevoFavorito: FormControl = this._fb.control('', Validators.required);
 
-  ngOnInit(): void {
+  get favoritosArr() {
+    return this.miFormulario.get('favoritos') as FormArray;
   }
 
+  constructor(private _fb: FormBuilder) {}
+
+  campoEsValido(campo: string) {
+    return (
+      this.miFormulario.controls[campo].errors &&
+      this.miFormulario.controls[campo].touched
+    );
+  }
+
+  agregarFavorito() {
+    if (this.nuevoFavorito.invalid) {
+      return;
+    }
+
+    this.favoritosArr.push(
+      this._fb.control(this.nuevoFavorito.value, Validators.required)
+    );
+    this.nuevoFavorito.reset();
+  }
+
+  borrar(index: number) {
+    this.favoritosArr.removeAt(index);
+  }
+
+  guardar() {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    console.log(this.miFormulario.value);
+  }
 }
